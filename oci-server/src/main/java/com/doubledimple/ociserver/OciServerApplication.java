658020fc0fa14b2f26d07f041fbbab2a731e55bf
doubleDimple <lovele.cn@gmail.com>
2024-09-21 08:15:41 +0800

    oci-start 项目架构初始化

---
+++ oci-server/src/main/java/com/doubledimple/ociserver/OciServerApplication.java
@@ -0,0 +1,13 @@
+package com.doubledimple.ociserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OciServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OciServerApplication.class, args);
+    }
+
+}
---
+++ oci-server/src/test/java/com/doubledimple/ociserver/OciServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doubledimple.ociserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OciServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
