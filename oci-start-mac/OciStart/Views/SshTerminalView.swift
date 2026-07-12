a7ce8b0f5992c5d2c1635d0ce78df730fb0e6c24
doubleDimple <lovele.cn@gmail.com>
2026-07-12 00:00:30 -0700

    mac os支持重构

--- oci-start-mac/OciStart/Views/SshTerminalView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-struct SshTerminalView: View {
-    @EnvironmentObject var appState: AppState
-    var body: some View {
-        EmbeddedPage(title: "SSH 终端", path: "/ssh/terminal")
-    }
-}
